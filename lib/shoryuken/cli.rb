5916fa979e6ee0ad38208cab33510af1a8f0ee98
Pablo Cantero <pablo@pablocantero.com>
2014-09-07 20:51:37 -0300

    Use IO.pipe to move the signal handling outside the trap context

--- lib/shoryuken/cli.rb
+++ lib/shoryuken/cli.rb
@@ -8,17 +8,41 @@ module Shoryuken
     include Singleton
 
     def run(options)
+      self_read, self_write = IO.pipe
+
+      %w(INT TERM USR1 USR2 TTIN).each do |sig|
+        trap sig do
+          self_write.puts(sig)
+        end
+      end
+
       setup_options(options)
 
       AWS.config options['aws']
-      @launcher = Shoryuken::Launcher.new(options).run
 
-      %w[INT QUIT HUP TERM].each do |signal|
-        # trap(signal) { @launcher.stop }
+      launcher = Shoryuken::Launcher.new(options)
+
+      begin
+        launcher.run
+
+        while readable_io = IO.select([self_read])
+          signal = readable_io.first[0].gets.strip
+          handle_signal(signal)
+        end
+      rescue Interrupt
+        logger.info 'Shutting down'
+        launcher.stop
+        exit(0)
       end
     end
 
     private
+
+    def handle_signal(sig)
+      Shoryuken.logger.debug "Got #{sig} signal"
+
+      raise Interrupt
+    end
 
     def setup_options(options)
       Shoryuken.options.merge!(options).deep_symbolize_keys
