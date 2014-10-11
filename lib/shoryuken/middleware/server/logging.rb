ruby-shoryuken/shoryuken

commit 573bd02133dd4e43ffdc84259816bb9387ad0455

Add started_at log back, it's useful to check which worker got stuck

lib/shoryuken/middleware/server/logging.rb
@@ -9,6 +9,8 @@ module Shoryuken
             begin
               started_at = Time.now
 
+              logger.inf("started at #{started}")
+
               yield
 
               total_time = elapsed(started_at)

commit 7a1ccd5f5e5b9ac1d5b42178f26f3f617f67869d

Fix typos introduced in the commit 573bd02

lib/shoryuken/middleware/server/logging.rb
@@ -9,7 +9,7 @@ module Shoryuken
             begin
               started_at = Time.now
 
-              logger.inf("started at #{started}")
+              logger.info { "started at #{started_at}" }
 
               yield
 
@@ -20,9 +20,9 @@ module Shoryuken
               end
 
               logger.info { "completed in: #{total_time} ms" }
-            rescue Exception
+            rescue => e
               logger.info { "failed in: #{elapsed(started_at)} ms" }
-              raise
+              raise e
             end
           end
         end