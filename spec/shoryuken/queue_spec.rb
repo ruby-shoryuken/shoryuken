0fcf4bb2511cef5fae262ca87bd40f5cb7e67769
Pablo Cantero <pablo@pablocantero.com>
2015-03-28 23:58:40 -0300

    Fix broken spec because of a typo

--- spec/shoryuken/queue_spec.rb
+++ spec/shoryuken/queue_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Shoryuken::Queue do
   let(:credentials) { Aws::Credentials.new('access_key_id', 'secret_access_key') }
-  let(:sqs)         { Aws::The::Client.new(stub_responses: true, credentials: credentials) }
+  let(:sqs)         { Aws::SQS::Client.new(stub_responses: true, credentials: credentials) }
   let(:queue_name)  { 'shoryuken' }
   let(:queue_url)   { 'https://eu-west-1.amazonaws.com:6059/123456789012/shoryuken' }
 
