666db11a6a879ad0a10561e9f42c97047ac45c42
Garry Shutler <garry@robustsoftware.co.uk>
2015-06-04 16:15:57 +0100

    Extracted queue polling strategy
    In order to be able to customize the order and manner in which Shoryuken
    polls SQS queues, the previous hard-coded strategy was extracted into a
    separate class.
    
    This meant that some of the method names were altered to be more generic
    (for example rebalance_queue_weight! became messages_received),
    otherwise this was a direct extraction.

--- lib/shoryuken/fetcher.rb
+++ lib/shoryuken/fetcher.rb
@@ -9,18 +9,6 @@ module Shoryuken
       @manager = manager
     end
 
-    def receive_messages(queue, limit)
-      # AWS limits the batch size by 10
-      limit = limit > FETCH_LIMIT ? FETCH_LIMIT : limit
-
-      options = (Shoryuken.options[:aws][:receive_message] || {}).dup
-      options[:max_number_of_messages] = limit
-      options[:message_attribute_names] = %w(All)
-      options[:attribute_names] = %w(All)
-
-      Shoryuken::Client.queues(queue).receive_messages options
-    end
-
     def fetch(queue, available_processors)
       watchdog('Fetcher#fetch died') do
         started_at = Time.now
@@ -28,23 +16,23 @@ module Shoryuken
         logger.debug { "Looking for new messages in '#{queue}'" }
 
         begin
-          batch = Shoryuken.worker_registry.batch_receive_messages?(queue)
+          batch = Shoryuken.worker_registry.batch_receive_messages?(queue.name)
           limit = batch ? FETCH_LIMIT : available_processors
 
           if (sqs_msgs = Array(receive_messages(queue, limit))).any?
             logger.info { "Found #{sqs_msgs.size} messages for '#{queue}'" }
 
             if batch
-              @manager.async.assign(queue, patch_sqs_msgs!(sqs_msgs))
+              @manager.async.assign(queue.name, patch_sqs_msgs!(sqs_msgs))
             else
-              sqs_msgs.each { |sqs_msg| @manager.async.assign(queue, sqs_msg) }
+              sqs_msgs.each { |sqs_msg| @manager.async.assign(queue.name, sqs_msg) }
             end
 
-            @manager.async.rebalance_queue_weight!(queue)
+            @manager.async.messages_present(queue)
           else
             logger.debug { "No message found for '#{queue}'" }
 
-            @manager.async.pause_queue!(queue)
+            @manager.async.queue_empty(queue)
           end
 
           logger.debug { "Fetcher for '#{queue}' completed in #{elapsed(started_at)} ms" }
@@ -55,10 +43,23 @@ module Shoryuken
 
         @manager.async.dispatch
       end
-
     end
 
     private
+
+    def receive_messages(queue, limit)
+      # AWS limits the batch size by 10
+      limit = limit > FETCH_LIMIT ? FETCH_LIMIT : limit
+
+      options = (Shoryuken.options[:aws][:receive_message] || {}).dup
+      options[:max_number_of_messages] = limit
+      options[:message_attribute_names] = %w(All)
+      options[:attribute_names] = %w(All)
+
+      options.merge!(queue.options)
+
+      Shoryuken::Client.queues(queue.name).receive_messages options
+    end
 
     def patch_sqs_msgs!(sqs_msgs)
       sqs_msgs.instance_eval do
