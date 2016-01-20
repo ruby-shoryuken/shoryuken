1c3c34a8cf555238001a7988d1b3b71dc95aaeff
Vokhmin Alexey V <avokhmin@gmail.com>
2016-01-20 20:10:24 +0300

    Compatibility with Rails 5

--- lib/shoryuken/extensions/active_job_adapter.rb
+++ lib/shoryuken/extensions/active_job_adapter.rb
@@ -15,45 +15,43 @@ module ActiveJob
     #
     #   Rails.application.config.active_job.queue_adapter = :shoryuken
     class ShoryukenAdapter
-      class << self
-        def enqueue(job) #:nodoc:
-          register_worker!(job)
+      def enqueue(job) #:nodoc:
+        register_worker!(job)
 
-          queue = Shoryuken::Client.queues(job.queue_name)
-          queue.send_message(message(job))
-        end
+        queue = Shoryuken::Client.queues(job.queue_name)
+        queue.send_message(message(job))
+      end
 
-        def enqueue_at(job, timestamp) #:nodoc:
-          register_worker!(job)
+      def enqueue_at(job, timestamp) #:nodoc:
+        register_worker!(job)
 
-          delay = (timestamp - Time.current.to_f).round
-          raise 'The maximum allowed delay is 15 minutes' if delay > 15.minutes
+        delay = (timestamp - Time.current.to_f).round
+        raise 'The maximum allowed delay is 15 minutes' if delay > 15.minutes
 
-          queue = Shoryuken::Client.queues(job.queue_name)
-          queue.send_message(message(job, delay_seconds: delay))
-        end
+        queue = Shoryuken::Client.queues(job.queue_name)
+        queue.send_message(message(job, delay_seconds: delay))
+      end
 
-        private
+      private
 
-        def message(job, options = {})
-          body = job.serialize
+      def message(job, options = {})
+        body = job.serialize
 
-          { message_body: body,
-            message_attributes: message_attributes }.merge(options)
-        end
+        { message_body: body,
+          message_attributes: message_attributes }.merge(options)
+      end
 
-        def register_worker!(job)
-          Shoryuken.register_worker(job.queue_name, JobWrapper)
-        end
+      def register_worker!(job)
+        Shoryuken.register_worker(job.queue_name, JobWrapper)
+      end
 
-        def message_attributes
-          @message_attributes ||= {
-            'shoryuken_class' => {
-              string_value: JobWrapper.to_s,
-              data_type: 'String'
-            }
+      def message_attributes
+        @message_attributes ||= {
+          'shoryuken_class' => {
+            string_value: JobWrapper.to_s,
+            data_type: 'String'
           }
-        end
+        }
       end
 
       class JobWrapper #:nodoc:
