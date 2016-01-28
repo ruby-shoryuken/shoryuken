3421a8e066947edbd6d5db851728b840d442d2fc
AkiraFukushima <akira.fukushima@crowdworks.co.jp>
2016-01-28 19:00:01 +0900

    Fix Hash methods define not only when got LoadError but also when method is undefined

--- lib/shoryuken/core_ext.rb
+++ lib/shoryuken/core_ext.rb
@@ -1,35 +1,47 @@
+module Shoryuken
+  module CoreExt
+    module StringifyKeys
+      def stringify_keys
+        keys.each do |key|
+          self[key.to_s] = delete(key)
+        end
+        self
+      end
+    end
+
+    module SymbolizeKeys
+      def symbolize_keys
+        keys.each do |key|
+          self[(key.to_sym rescue key) || key] = delete(key)
+        end
+        self
+      end
+    end
+
+    module DeepSymbolizeKeys
+      def deep_symbolize_keys
+        keys.each do |key|
+          value = delete(key)
+          self[(key.to_sym rescue key) || key] = value
+
+          value.deep_symbolize_keys if value.is_a? Hash
+        end
+        self
+      end
+    end
+  end
+end
+
 begin
   require 'active_support/core_ext/hash/keys'
   require 'active_support/core_ext/hash/deep_merge'
-  {}.stringify_keys
-  {}.symbolize_keys
-  {}.deep_symbolize_keys
-rescue LoadError, NoMethodError
-  class Hash
-    def stringify_keys
-      keys.each do |key|
-        self[key.to_s] = delete(key)
-      end
-      self
-    end if !{}.respond_to?(:stringify_keys)
+rescue LoadError
+end
 
-    def symbolize_keys
-      keys.each do |key|
-        self[(key.to_sym rescue key) || key] = delete(key)
-      end
-      self
-    end if !{}.respond_to?(:symbolize_keys)
-
-    def deep_symbolize_keys
-      keys.each do |key|
-        value = delete(key)
-        self[(key.to_sym rescue key) || key] = value
-
-        value.deep_symbolize_keys if value.is_a? Hash
-      end
-      self
-    end if !{}.respond_to?(:deep_symbolize_keys)
-  end
+class Hash
+  include Shoryuken::CoreExt::StringifyKeys unless method_defined?(:stringify_keys)
+  include Shoryuken::CoreExt::SymbolizeKeys unless method_defined?(:symbolize_keys)
+  include Shoryuken::CoreExt::DeepSymbolizeKeys unless method_defined?(:deep_symbolize_keys)
 end
 
 begin
