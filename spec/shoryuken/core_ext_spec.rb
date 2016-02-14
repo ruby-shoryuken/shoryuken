795675f79466e04e53ac97a4357f48da80769584
Pablo Cantero <pablo@pablocantero.com>
2016-02-14 10:18:02 -0200

    crowdworks-master Add specs to core extensions; Use the same standard from Hash ext for String ext

--- spec/shoryuken/core_ext_spec.rb
+++ spec/shoryuken/core_ext_spec.rb
@@ -1,6 +1,34 @@
 require 'spec_helper'
 
-describe 'core_ext' do
+RSpec.describe 'Core Extensions' do
+  describe Hash do
+    describe 'stringify_keys' do
+      it 'converts keys into strings' do
+        expect({ :key1 => 'value1', 'key2' => 'value2' }.stringify_keys).to eq('key1' => 'value1', 'key2' => 'value2')
+      end
+    end
+
+    describe 'symbolize_keys' do
+      it 'converts keys into strings' do
+        expect({ :key1 => 'value1', 'key2' => 'value2' }.symbolize_keys).to eq(:key1 => 'value1', key2: 'value2')
+      end
+    end
+
+    describe 'deep_symbolize_keys' do
+      it 'converts keys into strings' do
+        expect({ :key1 => 'value1',
+                 'key2' => 'value2',
+                 'key3' => {
+                   'key31' => { 'key311' => 'value311' },
+                   'key32' => 'value32' } }.deep_symbolize_keys).to eq({ :key1 => 'value1',
+                                                                         :key2 => 'value2',
+                                                                         :key3 => { :key31 =>
+                                                                                    { :key311 => 'value311' },
+                                                                                      :key32 => 'value32' } })
+      end
+    end
+  end
+
   describe String do
     describe '#constantize' do
       class HelloWorld; end
