a4b20f8769d74465cf02f030318cbbfd3abfae68
Pablo Cantero <pablo@pablocantero.com>
2016-02-14 10:18:02 -0200

    crowdworks-master Add specs to core extensions; Use the same standard from Hash ext for String ext

--- lib/shoryuken/core_ext.rb
+++ lib/shoryuken/core_ext.rb
@@ -1,5 +1,5 @@
 module Shoryuken
-  module CoreExt
+  module HashExt
     module StringifyKeys
       def stringify_keys
         keys.each do |key|
@@ -30,6 +30,21 @@ module Shoryuken
       end
     end
   end
+
+  module StringExt
+    module Constantize
+      def constantize
+        names = self.split('::')
+        names.shift if names.empty? || names.first.empty?
+
+        constant = Object
+        names.each do |name|
+          constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+        end
+        constant
+      end
+    end
+  end
 end
 
 begin
@@ -39,24 +54,16 @@ rescue LoadError
 end
 
 class Hash
-  include Shoryuken::CoreExt::StringifyKeys unless method_defined?(:stringify_keys)
-  include Shoryuken::CoreExt::SymbolizeKeys unless method_defined?(:symbolize_keys)
-  include Shoryuken::CoreExt::DeepSymbolizeKeys unless method_defined?(:deep_symbolize_keys)
+  include Shoryuken::HashExt::StringifyKeys unless method_defined?(:stringify_keys)
+  include Shoryuken::HashExt::SymbolizeKeys unless method_defined?(:symbolize_keys)
+  include Shoryuken::HashExt::DeepSymbolizeKeys unless method_defined?(:deep_symbolize_keys)
 end
 
 begin
   require 'active_support/core_ext/string/inflections'
 rescue LoadError
-  class String
-    def constantize
-      names = self.split('::')
-      names.shift if names.empty? || names.first.empty?
+end
 
-      constant = Object
-      names.each do |name|
-        constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
-      end
-      constant
-    end
-  end if !"".respond_to?(:constantize)
+class String
+  include Shoryuken::StringExt::Constantize unless method_defined?(:constantize)
 end
