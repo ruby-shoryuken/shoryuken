922449c799fc1fbf554d42c322a7032847dbd874
Pablo Cantero <pablo@pablocantero.com>
2016-09-02 17:16:21 -0300

    Complementary fix for #213. Pass body to worker_name for calls from AutoExtendVisibility

--- lib/shoryuken/middleware/server/auto_extend_visibility.rb
+++ lib/shoryuken/middleware/server/auto_extend_visibility.rb
@@ -7,7 +7,7 @@ module Shoryuken
         EXTEND_UPFRONT_SECONDS = 5
 
         def call(worker, queue, sqs_msg, body)
-          timer = auto_visibility_timer(queue, sqs_msg, worker.class)
+          timer = auto_visibility_timer(worker, queue, sqs_msg, body)
           begin
             yield
           ensure
@@ -21,31 +21,32 @@ module Shoryuken
           include Celluloid
           include Util
 
-          def auto_extend(queue, sqs_msg, worker_class)
+          def auto_extend(worker, queue, sqs_msg, body)
             queue_visibility_timeout = Shoryuken::Client.queues(queue).visibility_timeout
 
             every(queue_visibility_timeout - EXTEND_UPFRONT_SECONDS) do
               begin
                 logger.debug do
-                  "Extending message #{worker_name(worker_class, sqs_msg)}/#{queue}/#{sqs_msg.message_id}  " \
-                               "visibility timeout by #{queue_visibility_timeout}s."
+                  "Extending message #{worker_name(worker.class, sqs_msg, body)}/#{queue}/#{sqs_msg.message_id} " \
+                  "visibility timeout by #{queue_visibility_timeout}s."
                 end
 
                 sqs_msg.change_visibility(visibility_timeout: queue_visibility_timeout)
               rescue => e
                 logger.error do
-                  "Could not auto extend the message #{worker_class}/#{queue}/#{sqs_msg.message_id} " \
-                               "visibility timeout. Error: #{e.message}"
+                  'Could not auto extend the message ' \
+                  "#{worker_name(worker.class, sqs_msg, body)}/#{queue}/#{sqs_msg.message_id} " \
+                  "visibility timeout. Error: #{e.message}"
                 end
               end
             end
           end
         end
 
-        def auto_visibility_timer(queue, sqs_msg, worker_class)
-          return unless worker_class.auto_visibility_timeout?
+        def auto_visibility_timer(worker, queue, sqs_msg, body)
+          return unless worker.class.auto_visibility_timeout?
           @visibility_extender ||= MessageVisibilityExtender.new_link
-          @visibility_extender.auto_extend(queue, sqs_msg, worker_class)
+          @visibility_extender.auto_extend(worker, queue, sqs_msg, body)
         end
       end
     end
