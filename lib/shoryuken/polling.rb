160bba7382a9d101941f6f49ddc7567109a14817
Ash Tyndall <git@atyndall.net>
2016-12-16 09:33:39 +0800

    Restructure strategy based on feedback to be more clear and concise

--- lib/shoryuken/polling.rb
+++ lib/shoryuken/polling.rb
@@ -127,89 +127,71 @@ module Shoryuken
     class StrictPriority < BaseStrategy
 
       def initialize(queues)
-        # Mapping of queues to priority values
-        @queue_priorities = queues
-          .each_with_object(Hash.new(0)) { |queue, h| h[queue] += 1 }
-
-        # Priority ordering of the queues
-        @queue_order = @queue_priorities
-          .to_a
-          .sort_by { |queue, priority| -priority }
+        # Priority ordering of the queues, highest priority first
+        @initial_order = queues
+          .group_by { |q| q }
+          .sort_by { |q, qs| -qs.count }
           .map(&:first)
 
-        # Pause status of the queues
-        @queue_status = @queue_order
-          .each_with_object({}) { |queue, h| h[queue] = [true, nil] }
+        # Stores the queue ordering with the next queue as first element
+        @queue_order = @initial_order.dup
 
-        # Most recently used queue
-        @current_queue = nil
+        # Pause status of the queues, default to past time (unpaused)
+        @paused_until = queues
+          .each_with_object(Hash.new) { |queue, h| h[queue] = Time.at(0) }
       end
 
       def next_queue
-        unpause_queues
-        @current_queue = next_active_queue
-        return nil if @current_queue.nil?
-        QueueConfiguration.new(@current_queue, {})
+        next_queue = next_active_queue
+        next_queue.nil? ? nil : QueueConfiguration.new(next_queue, {})
       end
 
       def messages_found(queue, messages_found)
         if messages_found == 0
           # If no messages are found, we pause a given queue
-          pause(queue) 
+          pause(queue)
         else
-          # Reset the current queue when messages found to cause priorities to re-run
-          @current_queue = nil
+          # Reset the queue order to the initial ordering
+          @queue_order = @initial_order.dup
         end
       end
 
       def active_queues
-        @queue_status
-          .select { |_, status| status.first }
-          .map { |queue, _| [queue, @queue_priorities[queue]] }
+        @paused_until
+          .reject { |_, unpause_at| unpause_at > Time.now }
+          .map { |queue, _| [queue, @initial_order.reverse.find_index(queue) + 1] }
       end
 
       private
 
       def next_active_queue
-        return nil unless @queue_order.length > 0
+        now = Time.now
 
-        start = @current_queue.nil? ? 0 : @queue_order.index(@current_queue) + 1
-        i = 0
+        # Return nil if all queues are paused to prevent infinite loop
+        return nil if @paused_until.values.all? { |t| t > now }
 
-        # Loop through the queue order from the current queue until we find a
-        # queue that is next in line and is not paused
-        while true
-          queue = @queue_order[(start + i) % @queue_order.length]
-          active, delay = @queue_status[queue]
-          
-          i += 1
-          return queue if active
-          return nil if i >= @queue_order.length # Prevents infinite looping
+        # If any queues have unpaused since the last time we checked, reset the ordering
+        if @last_check && @paused_until.values.any? { |t| t > @last_check && t <= now }
+          @queue_order = @initial_order.dup
         end
+
+        @last_check = now
+
+        # `rotate!` through the queue list until we find an unpaused queue
+        begin
+          next_queue = @queue_order.first
+          unpause_at = @paused_until[next_queue]
+
+          @queue_order.rotate!
+        end while unpause_at > now
+
+        next_queue
       end
 
       def pause(queue)
         return unless delay > 0
-        @queue_status[queue] = [false, Time.now + delay]
+        @paused_until[queue] = Time.now + delay
         logger.debug "Paused '#{queue}'"
-      end
- 
-      def unpause_queues
-        # Modifies queue statuses for queues that are now unpaused
-        @queue_status = @queue_status.each_with_object({}) do |e, h|
-          queue, status = e
-          active, delay = status
-
-          h[queue] = if active
-            [true, nil]
-          elsif Time.now > delay
-            logger.debug "Unpaused '#{queue}'"
-            @current_queue = nil # Reset the check ordering on un-pause
-            [true, nil]
-          else
-            [false, delay]
-          end
-        end
       end
     end
   end
