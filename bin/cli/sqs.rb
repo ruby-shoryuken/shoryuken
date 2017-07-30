0db7d093c5d443ac84c35617dfbdebece5a40b22
Pablo Cantero <pablo@pablocantero.com>
2017-07-30 10:57:38 -0400

    Only set `FifoQueue` if not set already

--- bin/cli/sqs.rb
+++ bin/cli/sqs.rb
@@ -190,7 +190,7 @@ module Shoryuken
       method_option :attributes, aliases: '-a', type: :hash, default: {}
       def create(queue_name)
         attributes = options[:attributes]
-        attributes['FifoQueue'] = 'true' if queue_name.end_with?('.fifo')
+        attributes['FifoQueue'] ||= 'true' if queue_name.end_with?('.fifo')
 
         queue_url = sqs.create_queue(queue_name: queue_name, attributes: attributes).queue_url
 
