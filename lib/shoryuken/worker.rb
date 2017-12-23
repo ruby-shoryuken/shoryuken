7c8ee9b2ce08c5fb4cbb61cee1180c9eb5c944d2
Pablo Cantero <pablo@pablocantero.com>
2017-12-22 19:26:51 -0500

    Extract out worker executors

--- lib/shoryuken/worker.rb
+++ lib/shoryuken/worker.rb
@@ -4,31 +4,43 @@ module Shoryuken
       base.extend(ClassMethods)
     end
 
+    class DefaultExecutor
+      class << self
+        def perform_async(worker_class, body, options = {})
+          options[:message_attributes] ||= {}
+          options[:message_attributes]['shoryuken_class'] = {
+            string_value: worker_class.to_s,
+            data_type: 'String'
+          }
+
+          options[:message_body] = body
+
+          queue = options.delete(:queue) || worker_class.get_shoryuken_options['queue']
+
+          Shoryuken::Client.queues(queue).send_message(options)
+        end
+
+        def perform_in(worker_class, interval, body, options = {})
+          interval = interval.to_f
+          now = Time.now.to_f
+          ts = (interval < 1_000_000_000 ? (now + interval).to_f : interval)
+
+          delay = (ts - now).ceil
+
+          raise 'The maximum allowed delay is 15 minutes' if delay > 15 * 60
+
+          worker_class.perform_async(body, options.merge(delay_seconds: delay))
+        end
+      end
+    end
+
     module ClassMethods
       def perform_async(body, options = {})
-        options[:message_attributes] ||= {}
-        options[:message_attributes]['shoryuken_class'] = {
-          string_value: self.to_s,
-          data_type: 'String'
-        }
-
-        options[:message_body] = body
-
-        queue = options.delete(:queue) || get_shoryuken_options['queue']
-
-        Shoryuken::Client.queues(queue).send_message(options)
+        DefaultExecutor.perform_async(self, body, options)
       end
 
       def perform_in(interval, body, options = {})
-        interval = interval.to_f
-        now = Time.now.to_f
-        ts = (interval < 1_000_000_000 ? (now + interval).to_f : interval)
-
-        delay = (ts - now).ceil
-
-        raise 'The maximum allowed delay is 15 minutes' if delay > 15 * 60
-
-        perform_async(body, options.merge(delay_seconds: delay))
+        DefaultExecutor.perform_in(self, interval, body, options)
       end
 
       alias_method :perform_at, :perform_in
