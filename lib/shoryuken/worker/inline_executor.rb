77317205fc2c81e6cea4f4608f0be3ea0b946366
Pablo Cantero <pablo@pablocantero.com>
2017-12-22 19:41:44 -0500

    fake all other message attributes. See http://docs.aws.amazon.com/sdkforruby/api/Aws/SQS/Types/Message.html

--- lib/shoryuken/worker/inline_executor.rb
+++ lib/shoryuken/worker/inline_executor.rb
@@ -5,7 +5,15 @@ module Shoryuken
         def perform_async(worker_class, body, options = {})
           body = JSON.dump(body) if body.is_a?(Hash)
 
-          sqs_msg = OpenStruct.new(body: body)
+          sqs_msg = OpenStruct.new(
+            body: body,
+            attributes: nil,
+            md5_of_body: nil,
+            md5_of_message_attributes: nil,
+            message_attributes: nil,
+            message_id: nil,
+            receipt_handle: nil
+          )
 
           new.perform(sqs_msg, BodyParser.parse(self, sqs_msg))
         end
