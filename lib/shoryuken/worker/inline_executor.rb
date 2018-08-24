68fb55bf90980ec430befca71c8f3c7f02360881
Andrew Tongen <atongen@gmail.com>
2018-08-24 14:35:30 -0500

    Attempt to fix reek and rubo:cop: _again_
    Is this a joke?

--- lib/shoryuken/worker/inline_executor.rb
+++ lib/shoryuken/worker/inline_executor.rb
@@ -18,18 +18,12 @@ module Shoryuken
 
           parsed_body = BodyParser.parse(worker_class, sqs_msg)
           batch = worker_class.shoryuken_options_hash['batch']
-          arg0, arg1 = format_args(sqs_msg, body, batch)
-          worker_class.new.perform(arg0, arg1)
+          args = batch ? [[sqs_msg], [parsed_body]] : [sqs_msg, parsed_body]
+          worker_class.new.perform(*args)
         end
 
         def perform_in(worker_class, _interval, body, options = {})
           perform_async(worker_class, body, options)
-        end
-
-      private
-
-        def format_args(sqs_msg, parsed_body, batch)
-          batch ? [[sqs_msg], [parsed_body]] : [sqs_msg, parsed_body]
         end
       end
     end
