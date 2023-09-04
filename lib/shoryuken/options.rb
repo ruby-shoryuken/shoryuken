fabce07d179b131902c231f636a4480109296da7
Nikita Bulai <bulaj.nikita@gmail.com>
2023-09-04 12:37:37 +0300

    Allow setup custom exception handlers for failing jobs

--- lib/shoryuken/options.rb
+++ lib/shoryuken/options.rb
@@ -17,13 +17,14 @@ module Shoryuken
 
     attr_accessor :active_job_queue_name_prefixing, :cache_visibility_timeout, :groups,
                   :launcher_executor,
-                  :start_callback, :stop_callback, :worker_executor, :worker_registry
+                  :start_callback, :stop_callback, :worker_executor, :worker_registry, :exception_handlers
     attr_writer :default_worker_options, :sqs_client
     attr_reader :sqs_client_receive_message_opts
 
     def initialize
       self.groups = {}
       self.worker_registry = DefaultWorkerRegistry.new
+      self.exception_handlers = [DefaultExceptionHandler]
       self.active_job_queue_name_prefixing = false
       self.worker_executor = Worker::DefaultExecutor
       self.cache_visibility_timeout = false
