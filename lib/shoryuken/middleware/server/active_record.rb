ca8a8be0e9e8b2f7fcf423e62e77962a951e55b7
Tomasz Gieniusz <tomasz.gieniusz@gmail.com>
2023-12-05 20:12:32 +1100

    Backward compatibility with activerecord < 7.1

--- lib/shoryuken/middleware/server/active_record.rb
+++ lib/shoryuken/middleware/server/active_record.rb
@@ -5,7 +5,11 @@ module Shoryuken
         def call(*_args)
           yield
         ensure
-          ::ActiveRecord::Base.connection_handler.clear_active_connections!(:all)
+          if ::ActiveRecord.version >= Gem::Version.new('7.1')
+            ::ActiveRecord::Base.connection_handler.clear_active_connections!(:all)
+          else
+            ::ActiveRecord::Base.clear_active_connections!
+          end
         end
       end
     end
