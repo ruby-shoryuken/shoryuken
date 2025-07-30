0aefa51c4f91d337500fd3714aa5bb319c5d9bde
Maciej Mensfeld <maciej@mensfeld.pl>
2025-07-30 14:57:04 +0200

    remarks

--- lib/shoryuken/inline_message.rb
+++ lib/shoryuken/inline_message.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 module Shoryuken
+  # A high-performance alternative to OpenStruct for representing SQS messages.
+  #
+  # InlineMessage is a Struct-based implementation that provides the same interface
+  # as the previous OpenStruct-based message representation but with significantly
+  # better performance characteristics. It contains all the essential attributes
+  # needed to represent an Amazon SQS message within the Shoryuken framework.
   InlineMessage = Struct.new(
     :body,
     :attributes,
